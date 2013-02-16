f44cdaf99d7d4c9c752de50eafdd910bba7cb5aa
unknown <US084134@US194NBO5SYZZL1.us.tycoelectronics.com>
2013-02-16 10:59:27 -0500

    working map

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ["google-maps"]).
+    config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl});
+    $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: MapCtrl});
+    $routeProvider.otherwise({redirectTo: '/home'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* Controllers */
+
+function HomeCtrl($scope){
+    $scope.test = "testdata";
+}
+
+function MapCtrl($scope){
+    angular.extend($scope, {
+
+        /** the initial center of the map */
+        centerProperty: {
+            lat: 45,
+            lng: -73
+        },
+
+        /** the initial zoom level of the map */
+        zoomProperty: 8,
+
+        /** list of markers to put in the map */
+        markersProperty: [ {
+            latitude: 45,
+            longitude: -74
+        }],
+
+        // These 2 properties will be set when clicking on the map
+        clickedLatitudeProperty: null,
+        clickedLongitudeProperty: null
+    });
+
+
+}
+
+
